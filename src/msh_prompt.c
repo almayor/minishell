bcadd5c7f0e51ce993faa1ffae7d696b83131e58
almayor <mayorovme@gmail.com>
2020-09-18 04:40:25 +0300

    improved msh_prompt
    root directory is now displayed as "/"

--- src/msh_prompt.c
+++ src/msh_prompt.c
@@ -6,7 +6,7 @@
 /*   By: unite <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/13 20:51:05 by unite             #+#    #+#             */
-/*   Updated: 2020/09/14 04:17:04 by unite            ###   ########.fr       */
+/*   Updated: 2020/09/18 04:30:02 by unite            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,17 @@ const char	*msh_prompt(void)
 {
 	static char		prompt[MSH_PROMPT_BUFSIZE];
 	static char		pwd[PATH_MAX];
+	char			*dirname;
 	struct passwd	*pws;
 
 	pws = getpwuid(geteuid());
 	getcwd(pwd, PATH_MAX);
+	dirname = ft_strrchr(pwd, '/') ? ft_strrchr(pwd, '/') + 1 : pwd;
 	ft_snprintf(prompt, MSH_PROMPT_BUFSIZE,
 		"%s%s %s%s %s%s %s%s%s ",
 		ANSI_BGRN, pws->pw_name,
 		ANSI_BLU, "::",
-		ANSI_BMAG, ft_strrchr(pwd, '/') ? ft_strrchr(pwd, '/') + 1 : pwd,
+		ANSI_BMAG, *dirname ? dirname : "/",
 		ANSI_BLU, "$>", ANSI_RESET);
 	return (prompt);
 }
